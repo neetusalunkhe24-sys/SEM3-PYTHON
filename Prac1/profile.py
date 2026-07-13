a8aab3b8510173ce13ec9d45573b3d6f803b8482
neetusalunkhe24-sys <neetusalunkhe24@gmail.com>
2026-07-13 11:19:08 +0530

    Create profile.py

---
+++ Prac1/profile.py
@@ -0,0 +1,9 @@
+#1a. Print Profile 
+
+print("======= MY PROFILE =======")
+print("Name : Neetu Salunkhe")
+print("Age : 18")
+print("Course : Computer Science")
+print("Year : Second year")
+print("CLG Name : MVLU College")
+print("==========================")
